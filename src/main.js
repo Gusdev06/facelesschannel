207d5229452160b90668145fc2957502a3eaaecf
gusdev07 <gustavo.gomes@eshows.com.br>
2026-01-17 19:28:44 -0300

    last version

--- src/main.js
+++ src/main.js
@@ -146,7 +146,7 @@ async function getUserInput() {
       type: 'confirm',
       name: 'addTVOverlay',
       message: 'Add vintage TV effect overlay (particles and grain)?',
-      default: false
+      default: true
     },
     {
       type: 'number',
